4d21a82ab733e06d39da2b607d9884a6c3314adc
Tatooi Noyo <tatooi.noyo@outlook.com>
2025-12-20 21:42:09 +0800

    chore: Bump version code to 9 and version name to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "io.github.tatooinoyo.star.badge"
         minSdk = 31
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.3.2"
+        versionCode = 9
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
