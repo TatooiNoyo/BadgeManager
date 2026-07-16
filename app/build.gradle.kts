4b8981d638552465c511db66c7cbb2047a163e1f
Tatooi Noyo <tatooi.noyo@outlook.com>
2026-07-16 10:39:01 +0800

    build: Bump versionCode to 15 and versionName to 1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "io.github.tatooinoyo.star.badge"
         minSdk = 29
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.7.0"
+        versionCode = 15
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
