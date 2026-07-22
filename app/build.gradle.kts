e6c174d7b9270f8c02b5fba688ec19834b084253
Tatooi Noyo <tatooi.noyo@outlook.com>
2026-07-22 12:15:38 +0800

    build: bump version to 1.8.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "io.github.tatooinoyo.star.badge"
         minSdk = 29
         targetSdk = 35
-        versionCode = 17
-        versionName = "1.8.1"
+        versionCode = 18
+        versionName = "1.8.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
